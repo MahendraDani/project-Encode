6eb86ca35b58d0fcd865df9d29d5a1ae51ee0676
Mahendra Dani <mahendraunofficial09@gmail.com>
2023-05-12 11:39:07 +0530

    Added tailwindcss to the project

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return <div className="text-3xl bg-slate-400 font-mono">Homepage</div>;
 };
 
 export default Homepage;
